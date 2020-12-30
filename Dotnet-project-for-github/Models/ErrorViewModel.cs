4b1a0ad1ad78ecaf2080d4f79faa4ff704af1784
Ashwin Sridhar <ashwin.shridhar@gmail.com>
2020-12-29 21:27:52 -0600

    Initial commit

---
+++ Dotnet-project-for-github/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dotnet_project_for_github.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
